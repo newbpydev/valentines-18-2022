3a7bea659afd81a7b6747863a0885cd2011997e5
newbpydev <newbpydev@gmail.com>
2022-02-18 09:51:09 -0400

    ✨feat: add api fetch and bg and heart color change

--- index.pack.js
+++ index.pack.js
@@ -1,7 +1,34 @@
 const body = document.body;
+const heart = document.getElementById("heart");
 document.getElementById("btn").addEventListener("click", changeColor);
 
 async function changeColor() {
   // 1. Call Scrimba's color API (https://apis.scrimba.com/hexcolors/) to retrieve one random color.
   // 2. Use that color to update the background color of the body.
+  const count = 2;
+  const colors = [];
+
+  //* Fetching data from the scrimba colors api
+  try {
+    const response = await fetch(
+      `https://apis.scrimba.com/hexcolors/?count=${count}`,
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        data.colors.forEach((color) => {
+          colors.push(color.value);
+        })
+      })
+      .catch((err) => console.error(err));
+  } catch (e) {
+    console.log(e);
+  }
+
+  //* Change heart and bg colors
+  body.style.backgroundColor = colors[0];
+  heart.style.setProperty('--white', colors[1]);
+
 }
